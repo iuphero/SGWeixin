707f1f48d4c12b3f994ae359264efe178b77e3c1
Xfight <xfight9@gmail.com>
2014-05-23 10:18:59 +0800

    add response 1

--- wx.php
+++ wx.php
@@ -20,6 +20,18 @@ class MyWechat extends Wechat
         $this->conn = new PDO($text, $user, $pass, [PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8']);
     }
 
+    protected function sendPerson($person) {
+        $text = sprintf('%s(%s-%s) ，', $person['name'], $person['live_year'], $person['die_year']);
+        $text .= empty($person['style_name']) ? '' : '字：['.$person['style_name']."]。\r\n";
+        $text .= empty($person['native_place']) ? '' : '籍贯：'.$person['native_place']."。\r\n";
+        $text .= empty($person['office']) ? '' : '官至：'.$person['office']."。\r\n";
+        $text .= empty($person['history_dpt']) ? '': "历史简介:\r\n".$person['history_dpt']."\r\n";
+        // $text .= empty($person['history_dpt']) ? ''  :  "历史简介:\r\n".$person['history_dpt']."\r\n";
+        $text .= empty($person['novel_dpt']) ? '' : "演义简介：\r\n[".$person['novel_dpt']."]\r\n";
+        $skillText = sprintf('假想能力：\r\n武力%s, 智力%s, 统率%s, 政治%s, 魅力%s', $person['wl'], $person['zl'], $person['ts'], $person['zz'], $person['ml']);
+        $text .= $skillText;
+        $this->responseText($text);
+    }
     /**
      * 用户关注时触发，回复「欢迎关注」
      *
@@ -67,32 +79,28 @@ class MyWechat extends Wechat
         $content = $this->getRequest('content');
         if ($content == '1') {
              //随机发送一个人物
-            $text = 'hello';
-            $this->responseText($text);
-
+             $sql = 'SELECT name, style_name, sex, ts, wl, zl, zz, ml, native_place,
+          history_dpt, novel_dpt, assessment, office, live_year, die_year
+FROM `person` AS t1 JOIN (SELECT ROUND(RAND() * (SELECT MAX(id) FROM `person`)) AS id) AS t2
+WHERE t1.id >= t2.id and (t1.history_dpt is not null or t1.novel_dpt is not null)
+ORDER BY t1.id ASC LIMIT 1';
+            $results = $this->conn->query($sql);
+            $person = $results->fetch();
+            $this->sendPerson($person);
         } else {
             // $this->responseText('Today is a good day');
             $name = $content;
             $this->setDb('127.0.0.1',  'sanguo', 'root', 'wgmmla');
             $sql = sprintf("select name, style_name, sex, ts, wl, zl, zz, ml, native_place,
           history_dpt, novel_dpt, assessment, office, live_year, die_year
-          from person where name like '%s' or alias like '%s'  limit 1", $name . '%', $name . '%' );
+          from person where name = '%s' or alias = '%s'  limit 1", $name . '%', $name . '%' );
             $results = $this->conn->query($sql);
             if ($results->rowCount() == 0) {
-                $this->responseText('Sorry,小真没有查询到你要搜索的人物');
+                $this->responseText('Sorry,小真没有查询到你要搜索的人物,请输入正确的人名。\r\n回复１可以随机获得一个人物介绍。');
             }
             else {
                 $person = $results->fetch();
-                $text = sprintf('%s(%s-%s) ，', $person['name'], $person['live_year'], $person['die_year']);
-                $text .= empty($person['style_name']) ? '' : '字：['.$person['style_name']."]。\r\n";
-                $text .= empty($person['native_place']) ? '' : '籍贯：'.$person['native_place']."。\r\n";
-                $text .= empty($person['office']) ? '' : '官至：'.$person['office']."。\r\n";
-                $text .= empty($person['history_dpt']) ? '': "历史简介:\r\n".$person['history_dpt']."\r\n";
-                // $text .= empty($person['history_dpt']) ? ''  :  "历史简介:\r\n".$person['history_dpt']."\r\n";
-                $text .= empty($person['novel_dpt']) ? '' : "演义简介：\r\n[".$person['novel_dpt']."]\r\n";
-                $skillText = sprintf('假想能力：武力%s, 智力%s, 统率%s, 政治%s, 魅力%s', $person['wl'], $person['zl'], $person['ts'], $person['zz'], $person['ml']);
-                $text .= $skillText;
-                $this->responseText($text);
+                $this->sendPerson($person);
             }
         }
 
